106989ef95f60f1e15ff6ce1d4373d85b9646a6b
Dara Akitoye <daraakitoye1@gmail.com>
2021-09-15 05:19:59 +0000

    Adds character counter for tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+  $('textarea').keydown((event) => {
+    const charLength = event.target.value.length;
+
+    $('.counter').text(140 - charLength)
+
+    if (charLength > 140) {
+      $('.new-tweet output').css('color', 'crimson')
+    } else {
+      $('.new-tweet output').css('color', 'black')
+    }
+
+  })
+
+});
