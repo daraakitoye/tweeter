dde790e4834a6b404f69c79fd707a1cfb37ff1af
Dara Akitoye <daraakitoye1@gmail.com>
2021-09-20 19:08:12 +0000

    Some quick refactoring and commenting for better readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,75 +7,79 @@
 $(document).ready(function () {
 
 
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-      ,
-
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-
-    }
-
-  ]
-
-
-
-
+  //dynamically updates&styles tweets automatically
 
   const createTweetElement = (tweets) => {
     const $tweet = $('<article>').addClass('tweet');
 
     const HTML = `
    
-    <div class="test">
-    <img src="${tweets.user.avatars}"/>  <div class="username">${tweets.user.name}</div>
-      <label><strong>${tweets.user.handle}</strong></label>
-    </div>
-    <div name="text" class="tweet-text">${tweets.content.text}</div>
-    <div class="time-sent">${timeago.format(tweets['created_at'])}</div>
-    <div class="icon-container">
-    <i class="fa fa-flag" aria-hidden="true"></i>
-    <i class="fa fa-retweet" aria-hidden="true"></i>
+    <header>
+    <img src="${tweets.user.avatars}"/> 
+     <span class="name">${tweets.user.name}</span>
+      <span class="username">${tweets.user.handle}</span>
+    </header>
+    <span class="submitted-tweet">${tweets.content.text}</span>
+    <hr>
+    <footer>
+    <span class="time-sent">${timeago.format(tweets['created_at'])}</span>
+    <span class="icons">
     <i class="fa fa-heart" aria-hidden="true"></i>
-    </div>
+    <i class="fa fa-retweet" aria-hidden="true"></i>
+    <i class="fa fa-flag" aria-hidden="true"></i>
+    </span>
+    </footer>
  `
     $tweet.append(HTML);
     return $tweet
   }
 
+
+
+
+
+  // //renders tweet to page
   const renderTweets = function (userData) {
     for (const tweet of userData) {
       const $tweet = createTweetElement(tweet);
-      $('section.tweet-container').append($tweet);
+      $('#tweet-container').append($tweet);
     }
   };
 
 
-  renderTweets(tweetData);
+
+
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (fetchedTweets) {
+        renderTweets(fetchedTweets);
+      })
+  }
+  loadTweets();
+
+  //prevents page from redirecting on submit button
+  $('.new-tweet form').submit(function (event) {
+    event.preventDefault();
+    // const $input = $(this);
+    const $inputText = $(this).children('texts')
+    console.log($inputText)
+
+    // if (!$inputText) {
+    //   alert("Cannot submit empty text area. All tweets must be at least one character long.");
+    // } else if ($inputText.length > 140) {
+    //   alert("Too long: Cannot submit tweet greater than 140 characters.");
+    // } else {
+    const encoded = $('form').serialize();
+    $.ajax({ url: "/tweets/", method: 'POST', data: encoded })
+
+  });
+
+
 
 
   // Test / driver code (temporary). Eventually will get this from the server.
 
-  const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
 
 });
